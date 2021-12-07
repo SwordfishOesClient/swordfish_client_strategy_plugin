b5ec899ea61c1f409225bd2e60078802e03e9e3c
jhliu183 <87366004+jhliu183@users.noreply.github.com>
2021-12-07 11:11:00 +0800

    Add files via upload

--- python/strategy_sample.py
+++ python/strategy_sample.py
@@ -4,51 +4,67 @@ import sys
 if '.' not in sys.path:
     sys.path.append('.')
 from swordfish_api import strategy_engine
-from swordfish_api.client_api import CLIENT_API_NOTIFY_LEVEL_GENERAL, ClientApiMsgTypeT
-from swordfish_api.mds_struct import eMdsMsgTypeT, eMdsExchangeIdT
-from swordfish_api.oes_struct import eOesMarketIdT, eOesBuySellTypeT, eOesMsgTypeT, eOesOrdTypeT
+from swordfish_api.strategy_base import StrategyBase, do
+from swordfish_api.client_api import CLIENT_API_NOTIFY_LEVEL_GENERAL
+from swordfish_api.mds_struct import eMdsExchangeIdT
+from swordfish_api.oes_struct import eOesMarketIdT, eOesBuySellTypeT, eOesOrdTypeT
 
 
-MSG_TYPE_L1_SNAPSHOT = ClientApiMsgTypeT.CLIENT_API_MSG_BASE_MDS_START.value + \
-                       eMdsMsgTypeT.MDS_MSGTYPE_MARKET_DATA_SNAPSHOT_FULL_REFRESH.value
-MSG_TYPE_L2_SNAPSHOT = ClientApiMsgTypeT.CLIENT_API_MSG_BASE_MDS_START.value + \
-                       eMdsMsgTypeT.MDS_MSGTYPE_L2_MARKET_DATA_SNAPSHOT.value
-MSG_TYPE_ORDER_INSERT = ClientApiMsgTypeT.CLIENT_API_MSG_BASE_COUNTER_START.value + \
-                        eOesMsgTypeT.OESMSG_RPT_ORDER_INSERT.value
+class MyStrategy(StrategyBase):
+    def __init__(self):
+        self.msg_count = 0
 
-
-class P(object):
-    msg_count = 0
-
-
-def handle(msg):
-    msg_id = msg.msg_head.msg_id
-    if msg_id == MSG_TYPE_L1_SNAPSHOT or msg_id == MSG_TYPE_L2_SNAPSHOT:
-        P.msg_count += 1
-        if P.msg_count > 10000:
+    def on_snapshot(self, head, body):
+        self.msg_count += 1
+        if self.msg_count > 10000:
             rc = strategy_engine.quit()
             print(f"发送策略主动退出消息, 设置退出标志且返回-1 rc: {rc}")
             return -1
-        data = msg.msg_body.mkt_data_snapshot
-        if P.msg_count % 50 == 0:
-            if data.head.exchId == eMdsExchangeIdT.MDS_EXCH_SSE.value:
+        if self.msg_count % 50 == 0:
+            if head.exchId == eMdsExchangeIdT.MDS_EXCH_SSE.value:
                 mkt_id = eOesMarketIdT.OES_MKT_SH_ASHARE.value
             else:
                 mkt_id = eOesMarketIdT.OES_MKT_SZ_ASHARE.value
-            rc = strategy_engine.send_order(data.union.stock.SecurityID.decode(), mkt_id,
+            rc = strategy_engine.send_order(body.SecurityID.decode(), mkt_id,
                                             eOesBuySellTypeT.OES_BS_TYPE_BUY.value,
                                             eOesOrdTypeT.OES_ORD_TYPE_LMT.value, 100,
-                                            data.union.stock.TradePx)
+                                            body.TradePx)
             print(f"发送委托信息 rc: {rc}")
-    elif msg_id == MSG_TYPE_ORDER_INSERT:
-        strategy_engine.send_notify_msg(f"{strategy_engine.Engine.strategy_name} 收到交易数据", CLIENT_API_NOTIFY_LEVEL_GENERAL)
-    return 0
 
+    def on_l1_snapshot(self, head, body):
+        print("on_l1_snapshot")
+        self.on_snapshot(head, body)
 
-def main():
-    if strategy_engine.init(handle) == 0:
-        strategy_engine.do()
+    def on_l2_snapshot(self, head, body):
+        print("on_l2_snapshot")
+        self.on_snapshot(head, body)
+
+    def on_l2_trade(self, data):
+        print("on_l2_trade")
+
+    def on_l2_order(self, data):
+        print("on_l2_order")
+
+    def on_business_reject(self, head, body):
+        print("on_business_reject")
+
+    def on_order_insert(self, head, body):
+        print("on_order_insert")
+        strategy_engine.send_notify_msg(f"{strategy_engine.Engine.strategy_name} 收到交易数据",
+                                        CLIENT_API_NOTIFY_LEVEL_GENERAL)
+
+    def on_order_report(self, head, body):
+        print("on_order_report")
+
+    def on_trade_report(self, head, body):
+        print("on_trade_report")
+
+    def on_cash_variation(self, head, body):
+        print("on_cash_variation")
+
+    def on_stock_holding_variation(self, head, body):
+        print("on_stock_holding_variation")
 
 
 if __name__ == '__main__':
-    main()
+    do(MyStrategy())
